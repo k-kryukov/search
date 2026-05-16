7966f35610c8ca8a20bef6cd4c90a3bfe7ea863c
Konstantin Kryukov <kkryukov2013@gmail.com>
2026-05-16 16:14:35 +0300

    feat: added tokenizer logic

--- searcher/src/main.cpp
+++ searcher/src/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, world!" << std::endl;
+#include <tokenizer.hpp>
 
+/*
+
+Tasks on application start:
+1) Fetch index
+2) Fetch synonims file
+
+Tasks on query handle:
+1) Tokenize query
+2) Stemming? Lemmatization?
+3) Apply synonyms
+4) Go through reversed index
+5) Output results
+
+*/
+
+
+
+int main() {
+    auto tokenizer = std::make_unique<Tokenizer>();
+
+    std::string query{"Hello, world! How're you doing today?"};
+    
+    auto tokens = tokenizer->tokenize(query);
+
+    std::cout << "Hello, world!" << std::endl;
     return 0;
 }
